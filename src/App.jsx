4290490abaa070361d31c0c1847fd0f2acdb5298
Russ Perry <rperry1886@gmail.com>
2023-09-28 16:33:52 -0400

    Refactor: Change the rating count and price to use int instead of str

--- src/App.jsx
+++ src/App.jsx
@@ -13,27 +13,27 @@ function App() {
           img="./assets/card1.png"
           tag="sold out"
           rating="5.0"
-          ratingCount="6"
+          ratingCount={6}
           location="USA"
           title="Life lessons with Katie Zaferes"
-          price="136"
+          price={136}
         />
         <Card
           img="./assets/card2.png"
           tag="online"
           rating="5.0"
-          ratingCount="30"
+          ratingCount={30}
           location="USA"
           title="Learn wedding photography"
-          price="125"
+          price={125}
         />
         <Card
           img="./assets/card3.png"
           rating="4.8"
-          ratingCount="2"
+          ratingCount={2}
           location="USA"
           title="Group Mountain Biking"
-          price="50"
+          price={50}
         />
       </section>
     </>
