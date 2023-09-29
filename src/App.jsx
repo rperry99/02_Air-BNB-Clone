139c9599dfe45f612be19aaf7a706f52ff75171d
Russ Perry <rperry1886@gmail.com>
2023-09-29 14:11:23 -0400

    Refactor: Convert card data to JS file and use that instead of hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,28 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import cardData from './cardData';
 import './App.css';
 
 function App() {
+  const cardElements = cardData.map((card) => {
+    return (
+      <Card
+        img={card.img}
+        tag={card.tag}
+        rating={card.rating}
+        ratingCount={card.ratingCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
   return (
     <>
       <Navbar />
       <Hero />
-      <section className="card-group">
-        <Card
-          img="card1.png"
-          tag="sold out"
-          rating={5.0}
-          ratingCount={6}
-          location="USA"
-          title="Life lessons with Katie Zaferes"
-          price={136}
-        />
-        <Card
-          img="card2.png"
-          tag="online"
-          rating={5.0}
-          ratingCount={30}
-          location="Canada"
-          title="Learn wedding photography"
-          price={125}
-        />
-        <Card
-          img="card3.png"
-          rating={4.8}
-          ratingCount={2}
-          location="USA"
-          title="Group Mountain Biking"
-          price={50}
-        />
-      </section>
+      <section className="card-group">{cardElements}</section>
     </>
   );
 }
---
+++ src/cardData.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    img: 'card1.png',
+    tag: 'sold out',
+    rating: 5.0,
+    ratingCount: 6,
+    location: 'USA',
+    title: 'Life lessons with Katie Zaferes',
+    price: 136,
+  },
+  {
+    img: 'card2.png',
+    tag: 'online',
+    rating: 5.0,
+    ratingCount: 30,
+    location: 'Canada',
+    title: 'Learn Wedding Photography',
+    price: 125,
+  },
+  {
+    img: 'card3.png',
+    rating: 5.0,
+    ratingCount: 6,
+    location: 'USA',
+    title: 'Group Mountain Biking',
+    price: 50,
+  },
+];
